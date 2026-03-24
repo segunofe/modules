7de974dbcd113f75d0a4b81a2a95303b6d900d6a
Segun Ofe <ofesegunayodeji@gmail.com>
2026-03-24 12:55:44 -0700

    create app load balancer module

--- alb/main.tf
+++ alb/main.tf
@@ -3,8 +3,8 @@ resource "aws_lb" "application_load_balancer" {
   name                       = "${var.environment}-${var.project_name}-alb"
   internal                   = false
   load_balancer_type         = "application"
-  security_groups            = []
-  subnets                    = []
+  security_groups            = [var.alb_security_group_id]
+  subnets                    = [var.public_subnet_az1_id, var.public_subnet_az2_id]
   enable_deletion_protection = false
 
   tags = {
@@ -18,7 +18,7 @@ resource "aws_lb_target_group" "alb_target_group" {
   target_type = var.target_type
   port        = 80
   protocol    = "HTTP"
-  vpc_id      = 
+  vpc_id      = var.vpc_id
 
   health_check {
     healthy_threshold   = 5
@@ -55,7 +55,7 @@ resource "aws_lb_listener" "alb_https_listener" {
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = 
+  certificate_arn   = var.certificate_arn
 
   default_action {
     type             = "forward"
--- alb/outputs.tf
+++ alb/outputs.tf
@@ -1,15 +1,15 @@
 # ALB 
 output "alb_target_group_arn" {
   description = "The ARN of the ALB target group"
-  value       = 
+  value       = aws_lb_target_group.alb_target_group.arn
 }
 
 output "application_load_balancer_dns_name" {
   description = "The DNS name of the application load balancer"
-  value       = 
+  value       = aws_lb.application_load_balancer.dns_name
 }
 
 output "application_load_balancer_zone_id" {
   description = "The zone ID of the application load balancer"
-  value       = 
+  value       = aws_lb.application_load_balancer.zone_id
 }
