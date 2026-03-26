10c71a90c13175e6ea0ab4f0b9fbb81f488af3e7
Segun Ofe <ofesegunayodeji@gmail.com>
2026-03-26 08:14:22 -0700

    add 3 more outputs to ecs module

--- ecs/outputs.tf
+++ ecs/outputs.tf
@@ -1,5 +1,21 @@
 # ECS 
+# ECS 
+output "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  value       = aws_ecs_cluster.ecs_cluster.name
+}
+
+output "ecs_task_definition_name" {
+  description = "The name of the ECS task definition"
+  value       = aws_ecs_task_definition.ecs_task_definition.family
+}
+
 output "ecs_service" {
   description = "The ECS service"
   value       = aws_ecs_service.ecs_service
+}
+
+output "ecs_service_name" {
+  description = "The name of the ECS service"
+  value       = aws_ecs_service.ecs_service.name
 }
